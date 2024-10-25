4e079446ec6ad35b7a891ad31727e4aa083c7f38
Alejandro <alejandro.diazovejero@riberadeltajo.es>
2024-10-25 09:33:29 +0200

    Ultima actualización

--- php_012.php
+++ php_012.php
@@ -1,11 +1,22 @@
 <?php
-//función isset
-//Comprueba si una varibale está configurada, es decir, está declarada y no es NULL. En este caso devuelve 1
+//función isset Devuelve 1 si está definida la variable 0 en caso contrario.
+//Comprueba si una varibale está definida y no es NULL.
 //En caso contrario devuelve 0.
+//El valor NULL representa una variable SIN valor
 $var1= 4;
 if(isset($var1)){
     print("La variable var1 está inicializada y no es null. Su valor es: ".$var1);
 }else{
     print("La variable var1 no está inicializada");
+
+}
+
+unset($var1); 
+echo "<br>";
+$var2= null;
+if(isset($var1)){
+    print("La variable var2 está inicializada y no es null. Su valor es: ".$var2);
+}else{
+    print("La variable var2 no está inicializada y su valor es NULL");
 
 }
